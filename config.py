94704b1d534545af2e88d2dc10736ad374a626a3
HackxTrick <153048345+HackxTrick@users.noreply.github.com>
2024-07-11 23:42:03 +0530

    Update config.py
    Signed-off-by: HackxTrick <153048345+HackxTrick@users.noreply.github.com>

--- config.py
+++ config.py
@@ -14,12 +14,12 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 OWNER_ID = int(getenv("OWNER_ID"))
 
 PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
+START_IMG = getenv("START_IMG", "https://graph.org/file/18f2d373b00ef9c3d6f6e.jpg")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/INFINITE_EMPIRE")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/GAURAV_BOTS")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
 
